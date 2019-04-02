cb33569bec04cdf98a4637e1d01a83b33201b5c5
Thibault Gerrier <thibault.gerrier@sti2.at>
2019-04-02 17:56:19 +0200

    init

---
+++ cppsrc/xpath/xpathParser.cpp
@@ -0,0 +1,35 @@
+#include "xpathParser.h"
+#include <vector>
+#include <iostream>
+
+XpathParser::XpathParser(std::string xml, std::string iterator){
+    this->doc.load_string(xml.c_str());
+    pugi::xpath_query query_remote_tools(iterator.c_str());
+    this->nodes = query_remote_tools.evaluate_node_set(doc);
+}
+
+int XpathParser::getNumElems(){
+  return this->nodes.size();
+}
+
+std::vector<std::string> XpathParser::getData(int index, std::string selector){
+  try {
+    pugi::xpath_query query_remote_tools(selector.c_str());
+
+    pugi::xpath_node_set result_nodes = query_remote_tools.evaluate_node_set(this->nodes[index]);
+
+    std::vector<std::string> result;
+
+    for (pugi::xpath_node_set::const_iterator it = result_nodes.begin(); it != result_nodes.end(); ++it) {
+        pugi::xpath_node node = *it;
+        if(node.attribute()){
+          result.push_back(node.attribute().value());
+        }if(node.node()){
+          result.push_back(node.node().text().get());
+        }
+    }
+    return result;
+  } catch (const std::exception& e) {
+    return {};
+  }
+}
---
+++ cppsrc/xpath/xpathWrapper.cpp
@@ -0,0 +1,64 @@
+#include "xpathWrapper.h"
+
+Napi::FunctionReference XpathWrapper::constructor;
+
+Napi::Object XpathWrapper::Init(Napi::Env env, Napi::Object exports) {
+  Napi::HandleScope scope(env);
+
+  Napi::Function func = DefineClass(env, "XpathWrapper", {
+    InstanceMethod("getNumElems", &XpathWrapper::GetNumElems),
+    InstanceMethod("getData", &XpathWrapper::GetData),
+  });
+
+  constructor = Napi::Persistent(func);
+  constructor.SuppressDestruct();
+
+  exports.Set("XpathWrapper", func);
+  return exports;
+}
+
+XpathWrapper::XpathWrapper(const Napi::CallbackInfo& info) : Napi::ObjectWrap<XpathWrapper>(info)  {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+
+  int length = info.Length();
+  if (length != 2 || !info[0].IsString() || !info[1].IsString()) {
+    Napi::TypeError::New(env, "Invalid Input").ThrowAsJavaScriptException();
+  }
+
+  Napi::String xml = info[0].As<Napi::String>();
+  Napi::String iterator = info[1].As<Napi::String>();
+  this->xpathParser_ = new XpathParser(xml.ToString(), iterator.ToString());
+}
+
+Napi::Value XpathWrapper::GetNumElems(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+
+  int num = this->xpathParser_->getNumElems();
+
+  return Napi::Number::New(env, num);
+}
+
+
+Napi::Value XpathWrapper::GetData(const Napi::CallbackInfo& info) {
+  Napi::Env env = info.Env();
+  Napi::HandleScope scope(env);
+
+  if (info.Length() != 2 || !info[0].IsNumber() || !info[1].IsString()) {
+    Napi::TypeError::New(env, "Invalid input").ThrowAsJavaScriptException();
+  }
+
+  Napi::Number index = info[0].As<Napi::Number>();
+  Napi::String selector = info[1].As<Napi::String>();
+  std::vector<std::string> results = this->xpathParser_->getData(index, selector);
+
+  int length = results.size();
+
+  Napi::Array outputArray = Napi::Array::New(env, length);
+  for (int i = 0; i < length; i++) {
+    outputArray[i] = Napi::String::New(env, results[i]);
+  }
+  
+  return outputArray;
+}
