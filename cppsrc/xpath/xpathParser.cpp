550d090a9048c959beaa77d3618d5c9fdc5a8382
Thibault Gerrier <thibault.gerrier@sti2.at>
2019-04-15 16:15:44 +0200

    fix xpath which evaluate to string

--- cppsrc/xpath/xpathParser.cpp
+++ cppsrc/xpath/xpathParser.cpp
@@ -16,20 +16,33 @@ std::vector<std::string> XpathParser::getData(int index, std::string selector){
   try {
     pugi::xpath_query query_remote_tools(selector.c_str());
 
-    pugi::xpath_node_set result_nodes = query_remote_tools.evaluate_node_set(this->nodes[index]);
+    try {
+      pugi::xpath_node_set result_nodes = query_remote_tools.evaluate_node_set(this->nodes[index]);
 
-    std::vector<std::string> result;
+      std::vector<std::string> result;
 
-    for (pugi::xpath_node_set::const_iterator it = result_nodes.begin(); it != result_nodes.end(); ++it) {
-        pugi::xpath_node node = *it;
-        if(node.attribute()){
-          result.push_back(node.attribute().value());
-        }if(node.node()){
-          result.push_back(node.node().text().get());
-        }
+      for (pugi::xpath_node_set::const_iterator it = result_nodes.begin(); it != result_nodes.end(); ++it) {
+          pugi::xpath_node node = *it;
+          if(node.attribute()){
+            result.push_back(node.attribute().value());
+          } else if(node.node()){
+            result.push_back(node.node().text().get());
+          }
+      }
+      return result;
+    } catch(const std::exception& e) {
+      // not nodeset, try string
+      try {
+        auto str = query_remote_tools.evaluate_string(this->nodes[index]);
+        return { str };
+      } catch(const std::exception& e) {
+        std::cout << e.what() << std::endl;
+        return {};
+      }
     }
-    return result;
+
   } catch (const std::exception& e) {
+    std::cout << e.what() << std::endl;
     return {};
   }
 }
